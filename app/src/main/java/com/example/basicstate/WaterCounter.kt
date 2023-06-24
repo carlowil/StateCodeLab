382138fcf47ef8f322bba6b5562162c69f5cac6e
carlowil <matveysizov84@gmail.com>
2023-06-24 13:35:29 +0300

    rememberSaveable

--- app/src/main/java/com/example/basicstate/WaterCounter.kt
+++ app/src/main/java/com/example/basicstate/WaterCounter.kt
@@ -1,9 +1,7 @@
 package com.example.basicstate
 
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -11,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,29 +18,14 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier : Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {
+        var count by rememberSaveable {
             mutableStateOf(0)
         }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false }
-                )
-            }
             Text("You've had $count glasses.")
         }
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0 }, ) {
-                Text("Clear water count")
-            }
+        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
